5419cfe903f31f688554a1ad932b97191c4c8da9
mohammadnasr <nasrmohammad4804@gmail.com>
2024-10-09 22:21:26 +0330

    add file of traditional implementation that repeated in multiple case and possible to violate extensibility in future

---
+++ behavioral/state/src/problem/Main.java
@@ -0,0 +1,10 @@
+package problem;
+
+public class Main {
+    public static void main(String[] args) {
+        Canvas canvas = new Canvas(ToolType.BRUSH);
+
+        canvas.mouseDown();
+        canvas.mouseUp();
+    }
+}
---
+++ behavioral/state/src/problem/ToolType.java
@@ -0,0 +1,7 @@
+package problem;
+
+public enum ToolType {
+    SELECTION,
+    BRUSH,
+    ERASER;
+}
