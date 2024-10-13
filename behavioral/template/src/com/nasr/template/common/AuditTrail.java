8c5f5a0106b86e4b08abeb119516b28dbf3eeee1
mohammadnasr <nasrmohammad4804@gmail.com>
2024-10-13 12:47:53 +0330

    add common class for all

---
+++ behavioral/template/src/com/nasr/template/common/AuditTrail.java
@@ -0,0 +1,8 @@
+package com.nasr.template.common;
+
+public class AuditTrail {
+    public void record(String taskName){
+        System.out.println(" task with name of : "+taskName + " recorded !");
+    }
+
+}
